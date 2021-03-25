ab7789dd9453f48d1b472a458d64b501ef934bdd
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-25 12:55:58 +0530

    Added logic to use Select statement on EmployeePayroll Table

---
+++ src/main/java/com/employeepayrollJDBC/EmployeePayroll.java
@@ -0,0 +1,60 @@
+package com.employeepayrollJDBC;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
+
+public class EmployeePayroll {
+    private Connection getConnection() throws SQLException {
+        String jdbcurl = "jdbc:mysql://localhost:3306/employeepayroll?useSSL=false";
+        String username = "root";
+        String password = "surendra123";
+        Connection con;
+
+        System.out.println("Connecting to database : " + jdbcurl);
+        con = DriverManager.getConnection(jdbcurl, username, password);
+        System.out.println("Connection Successful : " + con);
+        return con;
+    }
+
+    public List<EmployeePayrollData> readData(){
+        String sql_query = "Select * from employee_payroll;";
+        List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        try{
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql_query);
+            while (resultSet.next()){
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                Date start = resultSet.getDate(3);
+                double salary = resultSet.getDouble(4);
+                String gender = resultSet.getString(5);
+                System.out.println("\n");
+                System.out.println("Id : " + id);
+                System.out.println("Name : " + name);
+                System.out.println("Start Date : " + start);
+                System.out.println("Salary : " + salary );
+                System.out.println("Gender : " + gender);
+
+                EmployeePayrollData employeePayroll = new EmployeePayrollData(resultSet.getInt(1),resultSet.getString(2),
+                        resultSet.getDate(3),resultSet.getDouble(4), resultSet.getString(5));
+                employeePayrollList.add(employeePayroll);
+            }
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+
+    public static void listdrivers(){
+        Enumeration<Driver> driverlist = DriverManager.getDrivers();
+        while(driverlist.hasMoreElements()){
+            Driver driverClass = (Driver) driverlist.nextElement();
+            System.out.println(" " + driverClass.getClass().getName());
+        }
+    }
+}
---
+++ src/main/java/com/employeepayrollJDBC/EmployeePayrollData.java
@@ -0,0 +1,59 @@
+package com.employeepayrollJDBC;
+
+import java.util.Date;
+
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public Date date;
+    public double salary;
+    public String gender;
+
+    public EmployeePayrollData(int id, String name, Date date, double salary, String gender) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.salary = salary;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
