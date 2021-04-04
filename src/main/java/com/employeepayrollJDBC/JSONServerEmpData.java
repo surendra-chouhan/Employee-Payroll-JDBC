532d2570c6e84f45f0da4dfe0560e6fb4fc3c90f
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-04 13:54:27 +0530

    Added logic to update record in JSON Server

--- src/main/java/com/employeepayrollJDBC/JSONServerEmpData.java
+++ src/main/java/com/employeepayrollJDBC/JSONServerEmpData.java
@@ -43,4 +43,5 @@ public class JSONServerEmpData {
                 ", salary=" + salary +
                 '}';
     }
+
 }
--- src/test/java/com/employeepayrollJDBC/JSONServerTest.java
+++ src/test/java/com/employeepayrollJDBC/JSONServerTest.java
@@ -35,17 +35,28 @@ public class JSONServerTest {
     public void givenEmployeeshouldRetrieveRecords(){
         JSONServerEmpData[] restAssureEmpData=getEmplist();
         System.out.println(restAssureEmpData);
-        Assert.assertEquals(8,restAssureEmpData.length);
+        Assert.assertEquals(10,restAssureEmpData.length);
     }
 
     @Test
     public void whenNewEmployeeisAddedShouldreturn201ResponseCode(){
         JSONServerEmpData[] jsonServerEmpData=getEmplist();
 
-        JSONServerEmpData jsonServerEmpData1=new JSONServerEmpData(9,"Suresh",8000);
+        JSONServerEmpData jsonServerEmpData1=new JSONServerEmpData(10,"Mahesh",7000);
 
         Response response=addEmployeeToJsonServer(jsonServerEmpData1);
         int statusCode= response.statusCode();
         Assert.assertEquals(201,statusCode);
+    }
+
+    @Test
+    public void whenEmployeeisUpdatedShouldReturn200ResponseCode() {
+        JSONServerEmpData[] serverEmpData = getEmplist();
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.header("Content-Type", "application/json");
+        requestSpecification.body("{\"name\":\"Dipesh\",\"salary\":\"789000\"}");
+        Response response = requestSpecification.put("/employees/update/6");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(200, statusCode);
     }
 }
